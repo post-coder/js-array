fb9177a257265e31e77e2ad571173366bb8bed5e
post-coder <gabrielspanu.dev@gmail.com>
2024-02-01 12:06:10 +0100

    esempio lista di nomi con input

--- js/script.js
+++ js/script.js
@@ -1,14 +1,46 @@
 
 // array di nomi
 const names = ["Gabriel", "Michele", "Miriam", "Ignazio", "Elisabetta"];
-// BEST PRACTICE: tutti i valori dentro l'array devono essere dello stesso tipo (stringhe, numeri, booleani ecc.)
-// BEST PRACTICE:  i nomi degli array DEVONO essere al plurale
 
-names.push("Daniele");
+// posso aggiungere elementi anche successivamente
+names.push("Arianna");
 
-for (let i = 0; i < names.length; i++ ) {
-    console.log(names[i]);
+// mostro in pagina la lista dei nomi
+
+// salvo la lista in una variabile
+const lista = document.querySelector("ol");
+
+
+// creo un ciclo che mi scorre tutti gli elementi dell'array names
+for(let i = 0; i < names.length; i++) {
+    
+    // per ognuno creo un elemento html
+    const newElement = document.createElement("li");
+
+    // ci scrivo dentro il nome corrispondente preso dall'array tramite il suo indice (i)
+    newElement.innerText = names[i];
+
+    // appendo l'elemento creato alla lista
+    lista.append(newElement);
 }
+// così sto visualizzando tutti i nomi, la lista essendo <ol> mostra già il numero relativo (per quanto sia +1)
 
+// mi salvo il pulsante
+const formButtonElement = document.getElementById("formButton");
 
-console.log(names); // ['Gabriel', 'Michele', 'Miriam', 'Ignazio', 'Roberto', 'Alan']
+// controllo quando ci effettuano il click
+formButtonElement.addEventListener("click",
+
+    function() {
+
+        // prendo ciò che è stato scritto dentro il campo di testo
+        const sceltaUtente = document.getElementById("nameNumber").value;
+
+        // ci inserisco dentro come testo il nome selezionato
+        // qui mi ricordo che l'indice che ha inserito l'utente nel campo di input in realtà 
+        // su js va diminuito di uno
+        document.getElementById("result").innerText = names[sceltaUtente-1];
+
+    }
+)
+
